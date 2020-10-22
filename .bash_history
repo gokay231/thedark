7521a0b851b50a23c1c13e6667c34f8a9cef08c8
gokay231 <73302224+gokay231@users.noreply.github.com>
2020-10-22 18:50:08 +0300

    Add files via upload

---
+++ .bash_history
@@ -0,0 +1,10 @@
+git prune
+git gc
+rm -rf .git
+rm -rf .git
+rm -rf .git
+rm -rf .git
+rm -rf .git
+rm -rf .git
+rm -gf .git
+rm -rf .git
---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  "token": "NzAyNjAxMDUzNDIxNjk5MTY0.XwAsTw.kJVcdxvXDae1-vpU7k02px9jU_0",
+
+  "dashboard" : {
+    "oauthSecret": "QsPduNsqM3FB09MoL71ulUziIortclxY", // botunuzun client secret tini buraya yazin
+    "callbackURL": "https://galaksia.glitch.me/callback",// burda login yapmak icin botunuzun Outh2 yazan yere gelip orda daki kizma yapistirip kaydedin
+    "sessionSecret": "g",
+    "domain": "https://galaksia.glitch.me",// buraya kc at alt yapi yerine yazdiysaniz kc-at-panel isimi ile degistirin
+    "port": 8000
+  },
+};
+
+module.exports = config;
